318c040966598d5c9801c56e0e09b6644f1fd39e
YaelZamora <ajedrezkasparov@gmail.com>
2024-01-02 17:37:12 -0600

    Initial Commit

---
+++ paths/pathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pathsApp.swift
+//  paths
+//
+//  Created by Yael Javier Zamora Moreno on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct pathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
